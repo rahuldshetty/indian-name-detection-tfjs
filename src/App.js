657fbc2b5285d30bf3081d2349181db7fd173898
Rahul <35rahuldshetty@gmail.com>
2021-02-08 21:57:16 +0530

    added images

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import * as tf from '@tensorflow/tfjs';
 function App() {
   const [result, setResult] = useState();
   const [model,setModel] = useState();
+  const [prob, setProb] = useState();
   const c2i = {" ": 0, "a": 1, "b": 2, "c": 3, "d": 4, "e": 5, "f": 6, "g": 7, "h": 8, "i": 9, "j": 10, "k": 11, "l": 12, "m": 13, "n": 14, "o": 15, "p": 16, "q": 17, "r": 18, "s": 19, "t": 20, "u": 21, "v": 22, "w": 23, "x": 24, "y": 25, "z": 26};
   const max_len = 10;
   const shape = [1, max_len];
@@ -31,10 +32,17 @@ function App() {
     let arr = vector_text(val);
     
     let tensor_pred = model.predict(tf.tensor2d(arr, shape)).dataSync();
+    setProb(tensor_pred);
     if(tensor_pred>0.5)
       setResult(1)   
     else
       setResult(0)
+  }
+
+  function probability_text(){
+    let gender = (prob > 0.5)? "Male" : "Female";
+    let val = (prob) > 0.5 ? prob : 1-prob;
+    return <p>Predicted {gender} with probability {(val * 100).toFixed(2)}%</p>
   }
 
   function get_gender_icon(gender){
@@ -43,9 +51,9 @@ function App() {
       url += "/"
     }
     if(gender==0){
-      return <img src={url + "female.png"}/>
+      return <div>{probability_text()}<img style={{width:"50", height:"50"}} src={url + "female.png"}/></div>
     }
-    else if(gender==1) return <img src={url + "male.png"}/>
+    else if(gender==1) return <div>{probability_text()}<img style={{width:"50", height:"50"}} src={url + "male.png"}/></div>
   }
 
   useEffect(() => {
@@ -55,6 +63,7 @@ function App() {
     })
     return () => {
       setModel();
+      setProb();
       setResult();
     }
   }, [])
@@ -62,11 +71,14 @@ function App() {
   return (
     <div className="App">
       <h1>Indian Name Gender Classification</h1>
-      <p>This is a React & Tensorflow.JS based project that uses Keras ML model to classify a given name as either femlae or male. The model has been trained on Indian Names.</p>
-      {get_gender_icon(result)}
+      <p>This is a React & Tensorflow.JS based project that uses Keras ML model to classify a given name as either female or male. The model has been trained on Indian Names.</p>
+      <a href="https://github.com/rahuldshetty/indian-name-detection-tfjs.git" style={{"textDecoration":"none"}}>
+        <i class="fa fa-github" > Source Code</i>
+      </a>
       <div style={{margin:"auto"}}>
         <input id="input_id" type="text" placeholder="Enter name..." onChange={handleSubmit}/>
       </div>
+      {get_gender_icon(result)}
     </div>
   );
 }
